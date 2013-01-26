66d8924eb5327873c7f07e3a907f133e8d43e9fa
ishigaya@mac <goodmoon.for.ergo@gmail.com>
2013-01-26 21:34:54 +0900

    Complete!!

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,41 @@
 package controllers
 
-import play.api._
+import play.api.data._
+import play.api.data.Forms._
 import play.api.mvc._
+import jp.t2v.lab.play20.auth._
 
-object Application extends Controller {
-  
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+object Application extends Controller  with LoginLogout with AuthConfigImple with Auth{
+  type LoginForm = Form[(String, String)]
+
+  def index = authorizedAction(models.Normal) { user => request =>
+    Ok(views.html.index(user.fullname))
   }
-  
+
+  def login = Action {
+    Ok(views.html.login(loginForm))
+  }
+
+  val loginForm: LoginForm = Form {
+    tuple("email" -> email, "password" -> nonEmptyText)
+  }
+
+
+  def authenticate = Action { implicit request =>
+    loginForm.bindFromRequest.fold(
+    formWithErrors => BadRequest(views.html.login(formWithErrors)),
+    {
+      case (email, password) =>
+        models.User.authenticate(email, password)
+          .map(_.id)
+          .map(gotoLoginSucceeded)
+          .getOrElse(
+          BadRequest(views.html.login(loginForm.fill((email, "")).withGlobalError("emailもしくはパスワードが間違っています。"))))
+    }
+    )
+  }
+
+  def logout = Action { implicit request =>
+    gotoLogoutSucceeded
+  }
 }
